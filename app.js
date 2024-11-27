ff5920add9a50b11d03a26802f2e3fc0620e5918
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-11-27 09:29:22 -0300

    Add dummy /version endpoint for deployment testing

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (_req, res) => {
+  res.send('1.0.0')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
