8a6c3d0ed5419521f680695949a308054f58f14d
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-11-27 09:58:54 -0300

    Update version to test deployment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('1.0.0')
+  res.send('1.0.1')
 })
 
 app.listen(PORT, () => {
